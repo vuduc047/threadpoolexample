e515fad7233be261b7a0e3d5483817e6f67461d3
Duke Vu (TECHVIFY.G1) <duke.vu@techvify.com.vn>
2022-12-22 15:51:06 +0700

    init

---
+++ src/main/java/com/tvf/crimsonlogic/CrimsonlogicApplication.java
@@ -0,0 +1,13 @@
+package com.tvf.crimsonlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimsonlogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimsonlogicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tvf/crimsonlogic/CrimsonlogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvf.crimsonlogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimsonlogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
